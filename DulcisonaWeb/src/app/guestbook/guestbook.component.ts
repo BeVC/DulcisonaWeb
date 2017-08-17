bd96e5c4f506bd39bed7e0c7b136931acc06e11c
BeVC <Bernard_Van_Couwenberghe@hotmail.com>
2017-08-17 20:48:59 +0200

    home button added some preppie work for guestbook

--- DulcisonaWeb/src/app/guestbook/guestbook.component.ts
+++ DulcisonaWeb/src/app/guestbook/guestbook.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-guestbook',
   templateUrl: './guestbook.component.html',
-  styleUrls: ['./guestbook.component.scss']
+  styleUrls: ['./guestbook.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GuestbookComponent implements OnInit {
 
