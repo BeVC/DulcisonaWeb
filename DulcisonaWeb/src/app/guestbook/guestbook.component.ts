ac72a0d9dab03c5ec1b201aaa5bfb2edfc45d7ec
BeVC <Bernard_Van_Couwenberghe@hotmail.com>
2017-08-23 19:50:32 +0200

    #7: guestbook partial

--- DulcisonaWeb/src/app/guestbook/guestbook.component.ts
+++ DulcisonaWeb/src/app/guestbook/guestbook.component.ts
@@ -1,16 +1,25 @@
 ﻿import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-guestbook',
-  templateUrl: './guestbook.component.html',
-  styleUrls: ['./guestbook.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-guestbook',
+    templateUrl: './guestbook.component.html',
+    styleUrls: ['./guestbook.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class GuestbookComponent implements OnInit {
 
-  constructor() { }
+    // UI PROPERTIES
+    showForm: boolean = false;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    //#region UI Events
+    uiOnSignGuestbookClicked() {
+        this.showForm = true;
+    }
+    //#endregion
 
 }
